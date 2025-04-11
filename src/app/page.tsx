1fac9dc81e55f5ddf1c79296a19d50ad20a9d584
TheGuschin <93988913+TheGuschin@users.noreply.github.com>
2025-04-11 09:10:43 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { SlSocialVkontakte, SlSocialYoutube } from "react-icons/sl";
 import { LiaTelegram } from "react-icons/lia";
+import "./wave.css"; // кастомные анимации
 
 export default function Page() {
+  const title = "DRAY FREERO";
+
   return (
     <>
-      <div className={`fixed -z-10 bg-[url(/background.png)] w-screen h-screen bg-fixed bg-center`} />
+      <div className="fixed -z-10 bg-[url(/background.png)] w-screen h-screen bg-fixed bg-center" />
       <div className="flex flex-col gap-3 h-screen justify-center items-center">
-        <h1 className="text-3xl md:text-6xl font-semibold typing">DRAY FREERO</h1>
+        <h1 className="text-3xl md:text-6xl font-semibold flex">
+          {title.split("").map((char, i) => (
+            <span
+              key={i}
+              className="wave-letter"
+              style={{ animationDelay: `${i * 0.1}s` }}
+            >
+              {char}
+            </span>
+          ))}
+        </h1>
         <span className="text-2xl md:text-3xl font-sans">networks/@drayfreero</span>
         <div className="flex gap-6">
           <a href="https://vk.com/drayfreero" target="_blank">
@@ -22,5 +35,5 @@ export default function Page() {
         </div>
       </div>
     </>
-  )
+  );
 }
